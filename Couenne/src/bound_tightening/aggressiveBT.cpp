c181308457547c5d9d6c30a702fdbc50f65c2179
Pietro Belotti <petr7b6@gmail.com>
2009-07-10 17:36:55 +0000

    fixed leaks in aggressiveBT.cpp

--- Couenne/src/bound_tightening/aggressiveBT.cpp
+++ Couenne/src/bound_tightening/aggressiveBT.cpp
@@ -1,5 +1,5 @@
-/* $Id$ */
-/*
+/* $Id$
+ *
  * Name:    aggressiveBT.cpp
  * Author:  Pietro Belotti
  * Purpose: aggressive bound tightening -- fake bounds in variables to
@@ -37,6 +37,7 @@ static double distanceToBound(int n, const double* xOrig,
   return Xdist;
 }
 
+
 // Aggressive Bound Tightening: for each variable, fake new bounds
 // [l,b] or [b,u] and apply bound tightening. If the interval is
 // fathomed on bounds or on infeasibility, the complementary bound
@@ -46,13 +47,13 @@ bool CouenneProblem::aggressiveBT (Bonmin::OsiTMINLPInterface *nlp,
 				   t_chg_bounds *chg_bds, 
 				   Bonmin::BabInfo * babInfo) const {
 
-  Jnlst()->Printf (J_ITERSUMMARY, J_BOUNDTIGHTENING, "Aggressive FBBT\n");
+  Jnlst () -> Printf (J_ITERSUMMARY, J_BOUNDTIGHTENING, "Aggressive FBBT\n");
 
   Bonmin::CouenneInfo* couInfo =
     dynamic_cast<Bonmin::CouenneInfo*>(babInfo);
 
   int  ncols  = nVars ();
-  bool retval = true;
+  bool retval = false;
 
   CouNumber
     *olb = new CouNumber [ncols],
@@ -100,167 +101,172 @@ bool CouenneProblem::aggressiveBT (Bonmin::OsiTMINLPInterface *nlp,
     CoinCopyN (nlp -> getColUpper (), nOrigVars_, upper);
 
     double *Y = new double [nvars];
+
     CoinFillN (Y, nvars, 0.);
     CoinCopyN (X (), nOrigVars_, Y);
 
     if (getIntegerCandidate (nlp -> getColSolution (), Y, lower, upper) < 0) {
+
       jnlst_ -> Printf(J_ITERSUMMARY, J_BOUNDTIGHTENING, "TODO: find NLP point in ABT failed\n");
+      retval = true;
+
+    } else {
+
+      //////////////////////////////////////////////////////
+
+      nlp -> setColLower    (lower);
+      nlp -> setColUpper    (upper);
+      nlp -> setColSolution (Y);
+
+      try {nlp -> initialSolve ();}
+      catch (Bonmin::TNLPSolver::UnsolvedError *E) {}
+    
       delete [] Y;
       delete [] lower;
       delete [] upper;
-      return true;
-    }
 
-    //////////////////////////////////////////////////////
-
-    nlp -> setColLower    (lower);
-    nlp -> setColUpper    (upper);
-    nlp -> setColSolution (Y);
-
-    try {
-      nlp -> initialSolve ();
-    }
-    catch (Bonmin::TNLPSolver::UnsolvedError *E) {
-    }
-    
-    delete [] Y;
-    delete [] lower;
-    delete [] upper;
-
-    if (nlp->isProvenOptimal()) {
-      closestSol = new Bonmin::CouenneInfo::NlpSolution 
-	(nOrigVars_, nlp->getColSolution(), nlp->getObjValue());
-      couInfo->addSolution(closestSol);
-      dist = 0.;
-    }
-    else {
-      jnlst_ -> Printf(J_ITERSUMMARY, J_BOUNDTIGHTENING, "TODO: NLP solve in ABT failed\n");
-      return true;
-    }
-  }
-
-  if (dist>1e10) {
-    jnlst_ -> Printf(J_ITERSUMMARY, J_BOUNDTIGHTENING, "TODO: Don't have point for ABT\n");
-    return true;
-  }
-
-  // X is now the NLP solution, but in a low-dimensional space. We
-  // have to get the corresponding point in higher dimensional space
-  // through getAuxs()
-
-  double *X = new double [ncols];
-  CoinCopyN (closestSol->solution(), nOrigVars_, X);
-  getAuxs (X);
-
-  // create a new, fictitious, bound bookkeeping structure
-  t_chg_bounds *f_chg = new t_chg_bounds [ncols];
-
-  if (Jnlst()->ProduceOutput(J_ITERSUMMARY, J_BOUNDTIGHTENING)) {
-    //    CouNumber cutoff = getCutOff ();
-    int       objind = Obj (0) -> Body  () -> Index ();
-    for (int i=0; i<nOrigVars_; i++)
-      Jnlst()->Printf(J_MOREVECTOR, J_BOUNDTIGHTENING,
-		      "   %2d %+20g [%+20g %+20g]\n",
-		      i, X [i], Lb (i), Ub (i));
-    Jnlst()->Printf(J_ITERSUMMARY, J_BOUNDTIGHTENING,
-		    "-------------\nAggressive BT. Current bound = %g, cutoff = %g, %d vars\n", 
-		    Lb (objind), getCutOff (), ncols);
-  }
-
-  int improved, second, iter = 0;
-
-  // Repeatedly fake tightening bounds on both sides of every variable
-  // to concentrate around current NLP point.
-  //
-  // MAX_ABT_ITER is the maximum # of outer cycles. Each call to
-  // fake_tighten in turn has an iterative algorithm for a
-  // derivative-free, uni-dimensional optimization problem on a
-  // monotone function.
-
-  do {
-
-    improved = 0;
-
-    // scan all variables
-    for (int i=0; i<ncols; i++) {
-
-      if (CoinCpuTime () > maxCpuTime_)
-	break;
-
-      int index = evalOrder (i);
-
-      if (Var (index) -> Multiplicity () <= 0) 
-	continue;
-
-      // AW: We only want to do the loop that temporarily changes
-      // bounds around the NLP solution only for those points from the
-      // NLP solution (no auxiliary vars)?
-
-      // PBe: if we do want that, index should be initialized as i, as
-      // evalOrder gives a variable index out of an array index.
-
-      // PBe: That makes a lot of sense when problems are really
-      // big. Instances arki000[24].nl spend a lot of time here
-
-      if ((nOrigVars_ < THRES_ABT_ORIG) || (index < nOrigVars_)) {
-
-	// if (index == objind) continue; // don't do it on objective function
-
-	Jnlst()->Printf(J_DETAILED, J_BOUNDTIGHTENING,
-			"------------- tighten left x%d\n", index);
-
-	// tighten on left
-	if ((X [index] >= Lb (index) + COUENNE_EPS)
-	    && ((improved = fake_tighten (0, index, X, olb, oub, chg_bds, f_chg)) < 0)) {
-	  retval = false;
-	  break;
-	}
-
-	second = 0;
-
-	Jnlst()->Printf(J_DETAILED, J_BOUNDTIGHTENING,
-			"------------- tighten right x%d\n", index);
-
-	// tighten on right
-	if ((X [index] <= Ub (index) - COUENNE_EPS)
-	    && ((second = fake_tighten (1, index, X, olb, oub, chg_bds, f_chg) < 0))) {
-	  retval = false;
-	  break;
-	}
-
-	improved += second;
+      if (nlp->isProvenOptimal()) {
+	closestSol = new Bonmin::CouenneInfo::NlpSolution 
+	  (nOrigVars_, nlp->getColSolution(), nlp->getObjValue());
+	couInfo->addSolution(closestSol);
+	dist = 0.;
+      }
+      else {
+	jnlst_ -> Printf(J_ITERSUMMARY, J_BOUNDTIGHTENING, "TODO: NLP solve in ABT failed\n");
+	retval = true;
       }
     }
-  } while (retval && (improved > THRES_ABT_IMPROVED) && (iter++ < MAX_ABT_ITER));
-
-  // store new valid bounds, or restore old ones if none changed
-  CoinCopyN (olb, ncols, Lb ());
-  CoinCopyN (oub, ncols, Ub ());
-
-  if (Jnlst()->ProduceOutput(J_ITERSUMMARY, J_BOUNDTIGHTENING)) {
-    Jnlst()->Printf(J_ITERSUMMARY, J_BOUNDTIGHTENING,"------------------\n");
-
-    if (!retval) Jnlst()->Printf(J_ITERSUMMARY, J_BOUNDTIGHTENING,
-				 "Couenne infeasible node from aggressive BT\n");
-
-    int objind = Obj (0) -> Body  () -> Index ();
-
-    Jnlst()->Printf(J_ITERSUMMARY, J_BOUNDTIGHTENING,
-		    "-------------\ndone Aggressive BT. Current bound = %g, cutoff = %g, %d vars\n", 
-		    Lb (objind), getCutOff (), ncols);
-
-    if (Jnlst()->ProduceOutput(J_DETAILED, J_BOUNDTIGHTENING))
-      for (int i=0; i<nOrigVars_; i++)
-	printf("   x%02d [%+20g %+20g]  | %+20g\n",
-	       i, Lb (i), Ub (i), X [i]);
-
-    if (Jnlst()->ProduceOutput(J_MOREDETAILED, J_BOUNDTIGHTENING))
-      for (int i=nOrigVars_; i<ncols; i++)
-	printf ("   w%02d [%+20g %+20g]  | %+20g\n", i, Lb (i), Ub (i), X [i]);
   }
 
-  delete [] X;
-  delete [] f_chg;
+  if (!retval) {
+
+    if (dist>1e10) {
+
+      jnlst_ -> Printf(J_ITERSUMMARY, J_BOUNDTIGHTENING, "TODO: Don't have point for ABT\n");
+      retval = true;
+
+    } else {
+
+      // X is now the NLP solution, but in a low-dimensional space. We
+      // have to get the corresponding point in higher dimensional space
+      // through getAuxs()
+
+      double *X = new double [ncols];
+      CoinCopyN (closestSol->solution(), nOrigVars_, X);
+      getAuxs (X);
+
+      // create a new, fictitious, bound bookkeeping structure
+      t_chg_bounds *f_chg = new t_chg_bounds [ncols];
+
+      if (Jnlst()->ProduceOutput(J_ITERSUMMARY, J_BOUNDTIGHTENING)) {
+	//    CouNumber cutoff = getCutOff ();
+	int       objind = Obj (0) -> Body  () -> Index ();
+	for (int i=0; i<nOrigVars_; i++)
+	  Jnlst()->Printf(J_MOREVECTOR, J_BOUNDTIGHTENING,
+			  "   %2d %+20g [%+20g %+20g]\n",
+			  i, X [i], Lb (i), Ub (i));
+	Jnlst()->Printf(J_ITERSUMMARY, J_BOUNDTIGHTENING,
+			"-------------\nAggressive BT. Current bound = %g, cutoff = %g, %d vars\n", 
+			Lb (objind), getCutOff (), ncols);
+      }
+
+      int improved, second, iter = 0;
+
+      // Repeatedly fake tightening bounds on both sides of every variable
+      // to concentrate around current NLP point.
+      //
+      // MAX_ABT_ITER is the maximum # of outer cycles. Each call to
+      // fake_tighten in turn has an iterative algorithm for a
+      // derivative-free, uni-dimensional optimization problem on a
+      // monotone function.
+
+      do {
+
+	improved = 0;
+
+	// scan all variables
+	for (int i=0; i<ncols; i++) {
+
+	  if (CoinCpuTime () > maxCpuTime_)
+	    break;
+
+	  int index = evalOrder (i);
+
+	  if (Var (index) -> Multiplicity () <= 0) 
+	    continue;
+
+	  // AW: We only want to do the loop that temporarily changes
+	  // bounds around the NLP solution only for those points from the
+	  // NLP solution (no auxiliary vars)?
+
+	  // PBe: if we do want that, index should be initialized as i, as
+	  // evalOrder gives a variable index out of an array index.
+
+	  // PBe: That makes a lot of sense when problems are really
+	  // big. Instances arki000[24].nl spend a lot of time here
+
+	  if ((nOrigVars_ < THRES_ABT_ORIG) || (index < nOrigVars_)) {
+
+	    // if (index == objind) continue; // don't do it on objective function
+
+	    Jnlst()->Printf(J_DETAILED, J_BOUNDTIGHTENING,
+			    "------------- tighten left x%d\n", index);
+
+	    // tighten on left
+	    if ((X [index] >= Lb (index) + COUENNE_EPS)
+		&& ((improved = fake_tighten (0, index, X, olb, oub, chg_bds, f_chg)) < 0)) {
+	      retval = false;
+	      break;
+	    }
+
+	    second = 0;
+
+	    Jnlst()->Printf(J_DETAILED, J_BOUNDTIGHTENING,
+			    "------------- tighten right x%d\n", index);
+
+	    // tighten on right
+	    if ((X [index] <= Ub (index) - COUENNE_EPS)
+		&& ((second = fake_tighten (1, index, X, olb, oub, chg_bds, f_chg) < 0))) {
+	      retval = false;
+	      break;
+	    }
+
+	    improved += second;
+	  }
+	}
+      } while (retval && (improved > THRES_ABT_IMPROVED) && (iter++ < MAX_ABT_ITER));
+
+      // store new valid bounds, or restore old ones if none changed
+      CoinCopyN (olb, ncols, Lb ());
+      CoinCopyN (oub, ncols, Ub ());
+
+      if (Jnlst()->ProduceOutput(J_ITERSUMMARY, J_BOUNDTIGHTENING)) {
+	Jnlst()->Printf(J_ITERSUMMARY, J_BOUNDTIGHTENING,"------------------\n");
+
+	if (!retval) Jnlst()->Printf(J_ITERSUMMARY, J_BOUNDTIGHTENING,
+				     "Couenne infeasible node from aggressive BT\n");
+
+	int objind = Obj (0) -> Body  () -> Index ();
+
+	Jnlst()->Printf(J_ITERSUMMARY, J_BOUNDTIGHTENING,
+			"-------------\ndone Aggressive BT. Current bound = %g, cutoff = %g, %d vars\n", 
+			Lb (objind), getCutOff (), ncols);
+
+	if (Jnlst()->ProduceOutput(J_DETAILED, J_BOUNDTIGHTENING))
+	  for (int i=0; i<nOrigVars_; i++)
+	    printf("   x%02d [%+20g %+20g]  | %+20g\n",
+		   i, Lb (i), Ub (i), X [i]);
+
+	if (Jnlst()->ProduceOutput(J_MOREDETAILED, J_BOUNDTIGHTENING))
+	  for (int i=nOrigVars_; i<ncols; i++)
+	    printf ("   w%02d [%+20g %+20g]  | %+20g\n", i, Lb (i), Ub (i), X [i]);
+      }
+
+      delete [] X;
+      delete [] f_chg;
+    }
+  }
+
   delete [] olb;
   delete [] oub;
 
