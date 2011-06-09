ac948bd23ffd3eddbe46c3e2dbe5fc0bc356c567
Timo Berthold <berthold@zib.de>
2011-06-09 21:00:05 +0000

    set time limit in SCIP

--- Couenne/src/heuristics/CouenneFPFindSolution.cpp
+++ Couenne/src/heuristics/CouenneFPFindSolution.cpp
@@ -12,6 +12,7 @@
 
 #include "CouenneFeasPump.hpp"
 #include "CouenneProblem.hpp"
+#include "CoinTime.hpp"
 
 using namespace Couenne;
 
@@ -50,7 +51,6 @@ double CouenneFeasPump::findSolution (double* sol) {
   /// solve MILP 
 
 #ifdef COIN_HAS_SCIP
-
    if (useSCIP_) {
 
      SCIP* scip;
@@ -68,11 +68,13 @@ double CouenneFeasPump::findSolution (double* sol) {
      const SCIP_Real* rhss;
      const int* indices;
 
+     SCIP_Real timelimit;
+
      double infinity;
      int nvars;
      int nconss;
 
-     printf ("USING SCIP\n");
+     int currentmilpmethod;
 
      // COUENNE_INFINITY , getInfinity()
 
@@ -114,6 +116,10 @@ double CouenneFeasPump::findSolution (double* sol) {
      // do not abort subproblem on CTRL-C
      SCIP_CALL( SCIPsetBoolParam(scip, "misc/catchctrlc", FALSE) );
 
+     // set time limit
+     timelimit = problem_ -> getMaxCpuTime () - CoinCpuTime ();
+     SCIP_CALL( SCIPsetRealParam(scip, "limits/time", timelimit) );
+
      // create problem instance in SCIP
      SCIP_CALL_ABORT( SCIPcreateProb(scip, "auxiliary FeasPump MILP", NULL, NULL, NULL, NULL, NULL, NULL, NULL) );
 
@@ -153,6 +159,7 @@ double CouenneFeasPump::findSolution (double* sol) {
         checkInfinity(scip, lhss[i], infinity);
         checkInfinity(scip, rhss[i], infinity);
 
+        // create an empty linear constraint
         SCIP_CALL_ABORT( SCIPcreateConsLinear(scip, &cons, consname, 0, NULL, NULL, lhss[i], rhss[i], 
               TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, FALSE) );
              
@@ -163,9 +170,128 @@ double CouenneFeasPump::findSolution (double* sol) {
            SCIP_CALL_ABORT( SCIPaddCoefLinear(scip, cons, vars[indices[j]], coeffs[j]) );
         }
 
+        // add constraint to SCIP
         SCIP_CALL_ABORT( SCIPaddCons(scip, cons) );
         SCIP_CALL_ABORT( SCIPreleaseCons(scip, &cons) );        
      }
+     
+     // determine the method to solve the MILP
+     if (milpMethod_ == 0 )
+     {
+        // TODO: add rule for automatic choice of currentmilpmethod
+        currentmilpmethod = 1;
+     }
+     else 
+        currentmilpmethod = milpMethod_; // use a fixed method to solve the MILP
+     
+     SCIPdebugMessage("using MILP method: %d\n",currentmilpmethod);
+
+     // tune SCIP differently, depending on the chosen method to solve the MILP
+     switch(currentmilpmethod)
+     {
+     case 1: // solve the MILP completely. SCIP's default setting should be best for this
+        break;
+
+     case 2: // solve the MILP quickly. Set limits on overall nodes and stall nodes (nodes without incumbent improvement)
+             // disable or cut down all procedures which are merely used for improving the dual bound, e.g., cuts
+        SCIP_CALL_ABORT( SCIPsetLongintParam(scip, "limits/stallnodes", 100) );
+        SCIP_CALL_ABORT( SCIPsetLongintParam(scip, "limits/nodes", 1000) );
+
+        // disable cutting plane separation 
+        SCIP_CALL_ABORT( SCIPsetSeparating(scip, SCIP_PARAMSETTING_OFF, TRUE) );
+        
+        // disable expensive presolving 
+        SCIP_CALL_ABORT( SCIPsetPresolving(scip, SCIP_PARAMSETTING_FAST, TRUE) );
+
+        // use aggressuve Heuristics 
+        SCIP_CALL_ABORT( SCIPsetHeuristics(scip, SCIP_PARAMSETTING_AGGRESSIVE, TRUE) );
+        
+        // use best estimate node selection 
+        if( SCIPfindNodesel(scip, "estimate") != NULL )
+        {
+           SCIP_CALL_ABORT( SCIPsetIntParam(scip, "nodeselection/estimate/stdpriority", INT_MAX/4) ); 
+        }
+        
+        // use inference branching 
+        if( SCIPfindBranchrule(scip, "inference") != NULL )
+        {
+           SCIP_CALL_ABORT( SCIPsetIntParam(scip, "branching/inference/priority", INT_MAX/4) );
+        }
+        
+        // disable conflict analysis 
+        SCIP_CALL_ABORT( SCIPsetBoolParam(scip, "conflict/useprop", FALSE) );
+        SCIP_CALL_ABORT( SCIPsetBoolParam(scip, "conflict/useinflp", FALSE) );
+        SCIP_CALL_ABORT( SCIPsetBoolParam(scip, "conflict/useboundlp", FALSE) );
+        SCIP_CALL_ABORT( SCIPsetBoolParam(scip, "conflict/usesb", FALSE) );
+        SCIP_CALL_ABORT( SCIPsetBoolParam(scip, "conflict/usepseudo", FALSE) );
+
+        break;
+
+     case 3: // solve the MILP with RENS. Disable most other features, enable RENS
+        SCIP_CALL_ABORT( SCIPsetLongintParam(scip, "limits/nodes", 1) );
+
+        // disable cutting plane separation 
+        SCIP_CALL_ABORT( SCIPsetSeparating(scip, SCIP_PARAMSETTING_OFF, TRUE) );
+        
+        // disable expensive presolving 
+        SCIP_CALL_ABORT( SCIPsetPresolving(scip, SCIP_PARAMSETTING_FAST, TRUE) );
+
+        // besides RENS, only use cheap heuristics 
+        SCIP_CALL_ABORT( SCIPsetHeuristics(scip, SCIP_PARAMSETTING_FAST, TRUE) );
+
+        // use inference branching 
+        if( SCIPfindBranchrule(scip, "inference") != NULL )
+        {
+           SCIP_CALL_ABORT( SCIPsetIntParam(scip, "branching/inference/priority", INT_MAX/4) );
+        }
+
+        // ensure that RENS is called
+        if( SCIPfindHeur(scip, "rens") != NULL )
+        {
+           SCIP_CALL_ABORT( SCIPsetIntParam(scip, "heuristics/rens/freq", 0) );
+           SCIP_CALL_ABORT( SCIPsetRealParam(scip, "heuristics/rens/minfixingrate", 0.0) );
+        }
+        break;
+
+     case 4: // solve the MILP with Feasibility Pump. Disable most other features, enable stage 3 for feaspump
+        SCIP_CALL_ABORT( SCIPsetLongintParam(scip, "limits/nodes", 1) );
+
+        // disable cutting plane separation 
+        SCIP_CALL_ABORT( SCIPsetSeparating(scip, SCIP_PARAMSETTING_OFF, TRUE) );
+        
+        // disable expensive presolving 
+        SCIP_CALL_ABORT( SCIPsetPresolving(scip, SCIP_PARAMSETTING_FAST, TRUE) );
+
+        // besides feaspump, only use cheap heuristics 
+        SCIP_CALL_ABORT( SCIPsetHeuristics(scip, SCIP_PARAMSETTING_FAST, TRUE) );
+
+        // use inference branching 
+        if( SCIPfindBranchrule(scip, "inference") != NULL )
+        {
+           SCIP_CALL_ABORT( SCIPsetIntParam(scip, "branching/inference/priority", INT_MAX/4) );
+        }
+
+        // ensure that feasibility pump is called
+        if( SCIPfindHeur(scip, "feaspump") != NULL )
+        {
+           SCIP_CALL_ABORT( SCIPsetIntParam(scip, "heuristics/feaspump/freq", 0) );
+           SCIP_CALL_ABORT( SCIPsetIntParam(scip, "heuristics/feaspump/maxsols", -1) );
+           (void) SCIPsetBoolParam(scip, "heuristics/feaspump/stage3", TRUE);
+           (void) SCIPsetBoolParam(scip, "heuristics/feaspump2/stage3", TRUE);
+        }
+
+        break;
+
+     default:
+        break;
+     }
+  /// 1. Solve a MILP relaxation with Manhattan distance as objective
+  /// 2. Partially solve the MILP with emphasis on good solutions
+  /// 3. Apply RENS to 1
+  /// 4. Use Objective FP 2.0 for MILPs
+  /// 5. round-and-propagate
+  /// 6. choose from pool, see 4
+  /// 7. random perturbation
 
           
      // solve the MILP
