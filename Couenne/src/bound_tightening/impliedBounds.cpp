10babc597ec28dbc9292dc7cf4ffddf3907ed95a
Pietro Belotti <petr7b6@gmail.com>
2012-02-11 14:11:00 +0000

    merged changes from trunk:833 -- fix bug in impliedBound-exprPow

--- Couenne/src/bound_tightening/impliedBounds.cpp
+++ Couenne/src/bound_tightening/impliedBounds.cpp
@@ -42,7 +42,7 @@ int CouenneProblem::impliedBounds (t_chg_bounds *chg_bds) const {
     Jnlst()->Printf(Ipopt::J_DETAILED, J_BOUNDTIGHTENING,"  backward =====================\n  ");
     int j=0;
     for (int i=0; i < nVars (); i++) 
-      if (variables_ [i] -> Multiplicity () >= 0) {
+      if (variables_ [i] -> Multiplicity () > 0) {
 	Jnlst()->Printf(Ipopt::J_MOREVECTOR, J_BOUNDTIGHTENING,
 			"x_%03d [%+10g %+10g] ", i, 
 			domain_.lb (i),
