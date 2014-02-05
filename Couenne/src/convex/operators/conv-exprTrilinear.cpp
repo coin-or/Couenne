6fa6a0cfdaf2e7f1380e2886d44e0760446fa7d2
Pietro Belotti <petr7b6@gmail.com>
2014-02-05 01:44:35 +0000

    improved readability of bounds in exprTrilinear

--- Couenne/src/convex/operators/conv-exprTrilinear.cpp
+++ Couenne/src/convex/operators/conv-exprTrilinear.cpp
@@ -34,14 +34,16 @@ void exprTrilinear::getBounds (expression *&lb, expression *&ub) {
 
 	int indexTerm = i0*8 + i1*4 + i2*2;
 
-	arglistMax [indexTerm] = new exprCopy (new exprTrilinear (new exprClone (i0 ? ubA [0] : lbA [0]),
-								  new exprClone (i1 ? ubA [1] : lbA [1]),
-								  new exprClone (i2 ? ubA [2] : lbA [2]))); // saves value to be retrieved later for computation
+	expression *product = new exprTrilinear (new exprClone (i0 ? ubA [0] : lbA [0]),
+						 new exprClone (i1 ? ubA [1] : lbA [1]),
+						 new exprClone (i2 ? ubA [2] : lbA [2]));
 
-	arglistMin [indexTerm] = new exprClone (arglistMax [indexTerm]);
+	arglistMax [indexTerm] = new exprCopy (product); // saves value to be retrieved later for computation
+
+	arglistMin [indexTerm] = new exprCopy (new exprClone (product));
 
 	arglistMax [indexTerm + 1] = new exprStore (arglistMax [indexTerm]); // evaluated at the end, safe to just copy
-	arglistMin [indexTerm + 1] = new exprStore (arglistMax [indexTerm]); // evaluated at the end, safe to just copy	
+	arglistMin [indexTerm + 1] = new exprStore (arglistMin [indexTerm]); // evaluated at the end, safe to just copy	
       }
 
   lb = new exprMin (arglistMin, 16);
