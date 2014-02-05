c8a3747ab13cea53d508e303a54a3cd4621bab94
Pietro Belotti <petr7b6@gmail.com>
2014-02-05 01:45:36 +0000

    merge changeset 1068 from stable/0.4 (fix of bounds in exprTrilinear)

--- Couenne/src/convex/operators/conv-exprTrilinear.cpp
+++ Couenne/src/convex/operators/conv-exprTrilinear.cpp
@@ -34,14 +34,16 @@ void exprTrilinear::getBounds (expression *&lb, expression *&ub) {
 
 	int indexTerm = i0*8 + i1*4 + i2*2;
 
-	arglistMax [indexTerm] = new exprTrilinear (new exprClone (i0 ? ubA [0] : lbA [0]),
-						    new exprClone (i1 ? ubA [1] : lbA [1]),
-						    new exprClone (i2 ? ubA [2] : lbA [2]));
+	expression *product = new exprTrilinear (new exprClone (i0 ? ubA [0] : lbA [0]),
+						 new exprClone (i1 ? ubA [1] : lbA [1]),
+						 new exprClone (i2 ? ubA [2] : lbA [2]));
 
-	arglistMin [indexTerm] = new exprClone (arglistMax [indexTerm]);
+	arglistMax [indexTerm] = new exprCopy (product); // saves value to be retrieved later for computation
+
+	arglistMin [indexTerm] = new exprCopy (new exprClone (product));
 
 	arglistMax [indexTerm + 1] = new exprStore (arglistMax [indexTerm]); // evaluated at the end, safe to just copy
-	arglistMin [indexTerm + 1] = new exprStore (arglistMax [indexTerm]); // evaluated at the end, safe to just copy	
+	arglistMin [indexTerm + 1] = new exprStore (arglistMin [indexTerm]); // evaluated at the end, safe to just copy	
       }
 
   lb = new exprMin (arglistMin, 16);
@@ -75,4 +77,17 @@ void exprTrilinear::getBounds (CouNumber &lb, CouNumber &ub) {
 	if (curbound < lb) lb = curbound;
 	if (curbound > ub) ub = curbound;
       }
+
+  bool isInt = true;
+
+  for (int i=0; i<3; i++)
+    if (!(arglist_ [i] -> isInteger ())) {
+      isInt = false;
+      break;
+    }
+
+  if (isInt) {
+    lb = ceil  (lb - COUENNE_EPS);
+    ub = floor (ub + COUENNE_EPS);
+  }
 }
