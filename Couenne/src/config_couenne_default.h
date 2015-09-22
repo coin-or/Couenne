36e7bfe7f19c9a3ae33725b9cdec023a06eb42ac
Ted Ralphs <ted@lehigh.edu>
2015-09-22 23:27:20 +0000

    temporarily committing release candidate

--- Couenne/src/config_couenne_default.h
+++ Couenne/src/config_couenne_default.h
@@ -5,13 +5,13 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define COUENNE_VERSION      "0.5"
+#define COUENNE_VERSION "0.5.6"
 
 /* Major Version number of project */
-#define COUENNE_VERSION_MAJOR   0
+#define COUENNE_VERSION_MAJOR 0
 
 /* Minor Version number of project */
-#define COUENNE_VERSION_MINOR   5
+#define COUENNE_VERSION_MINOR 5
 
 /* Release Version number of project */
-#define COUENNE_VERSION_RELEASE 9999
+#define COUENNE_VERSION_RELEASE 6
