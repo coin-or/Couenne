6457baa6c0d0e3bee0287c5a4727b391f2481ecd
Pietro Belotti <petr7b6@gmail.com>
2015-11-24 23:34:57 +0000

    n/a

--- Couenne/src/readnl/readnl.cpp
+++ Couenne/src/readnl/readnl.cpp
@@ -391,7 +391,7 @@ int CouenneProblem::readnl (const ASL *asl) {
       case COUENNE_LE:  addLEConstraint  (body, new exprConst (ub)); break;
       case COUENNE_GE:  addGEConstraint  (body, new exprConst (lb)); break;
       case COUENNE_RNG: addRNGConstraint (body, new exprConst (lb), 
-					      new exprConst (ub)); break;
+                                                new exprConst (ub)); break;
       default: printf ("Could not recognize constraint\n"); return -1;
       }
 
@@ -448,7 +448,7 @@ int CouenneProblem::readnl (const ASL *asl) {
 
   for (register int i=n_var; i--;) 
 
-    if (X0 && havex0 [i]) X (i) = X0 [i]; 
+    if (X0 && havex0 [i]) X (i) = X0 [i];
 
     else {
 
