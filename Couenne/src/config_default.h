8bb66119be1d3a0526838d5ff76eb28c4d1dec9e
Stefan Vigerske <svigerske@gams.com>
2020-01-29 16:10:31 +0000

    setup config headers, incl COUENNELIB_EXPORT

--- Couenne/src/config_default.h
+++ Couenne/src/config_default.h
@@ -2,10 +2,20 @@
 /* include the COIN-OR-wide system specific configure header */
 #include "configall_system.h"
 
+/* this needs to come before the include of config_ipopt_default.h */
+#ifndef COUENNELIB_EXPORT
+#ifdef _WIN32
+/* assuming we build an Ipopt DLL */
+#define COUENNELIB_EXPORT __declspec(dllexport)
+#else
+#define COUENNELIB_EXPORT
+#endif
+#endif
+
 /* include the public project specific macros */
 #include "config_couenne_default.h"
 
 /***************************************************************************/
-/*             HERE DEFINE THE PROJECT SPECIFIC MACROS                     */
+/*        HERE DEFINE THE PROJECT SPECIFIC PRIVATE MACROS                  */
 /*    These are only in effect in a setting that doesn't use configure     */
 /***************************************************************************/
